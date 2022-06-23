5a1609eb67e8fc7c3636c9220082e3e8ae0a0986
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-06-23 15:34:38 -0300

    feat: created user model

---
+++ src/models/userModel.ts
@@ -0,0 +1,31 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import userQueries from '../queries/userQueries';
+// import IUser from '../interfaces/user';
+
+interface IUserWithId extends IUser {
+  id: number,
+}
+
+class UserModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async findUser(username: string, password: string): Promise<IUserWithId[]> {
+    const [response] = await this.connection
+      .execute(userQueries.findUser, [username, password]);
+
+    return response as IUserWithId[];
+  }
+
+  public async create({ username, classe, level, password }: IUser) {
+    const [{ insertId }] = await this.connection
+      .execute<ResultSetHeader>(userQueries.createUser, [username, classe, level, password]);
+
+    return insertId;
+  }
+}
+
+export default UserModel;
