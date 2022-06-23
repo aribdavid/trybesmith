1fe28b13131885e5f12a0875cf955cf97ff70f85
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-06-23 15:36:38 -0300

    feat: created product model

---
+++ src/models/productModel.ts
@@ -0,0 +1,30 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import IProducts from '../interfaces/products';
+import productQueries from '../queries/productQueries';
+
+class ProductsModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async getAll(): Promise<IProducts[]> {
+    const [response] = await this.connection.execute(productQueries.getAll);
+
+    return response as IProducts[];
+  }
+
+  public async create(name: string, amount: string): Promise<number> {
+    const [{ insertId }] = await this
+      .connection.execute<ResultSetHeader>(productQueries.createProduct, [name, amount]);
+
+    return insertId;
+  }
+
+  public async updateOrder(idOrder: number, idProduct: number) {
+    await this.connection.execute(productQueries.updateOrder, [idOrder, idProduct]);
+  }
+}
+
+export default ProductsModel;
