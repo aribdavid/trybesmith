ad3d0455865dd6df548f4b868aa7caf4a2eea19b
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-06-23 15:26:19 -0300

    feat: created order sql queries

---
+++ src/queries/orderQueries.ts
@@ -0,0 +1,16 @@
+const getAll = `
+  SELECT
+    o.id as id,
+    o.userId as userId,
+    pr.id as productsIds
+  FROM Trybesmith.Orders as o
+  INNER JOIN
+  Trybesmith.Products AS pr ON pr.orderId = o.id;
+`;
+
+const create = 'INSERT INTO Trybesmith.Orders (userId) VALUES (?)';
+
+export default {
+  getAll,
+  create,
+}; 
