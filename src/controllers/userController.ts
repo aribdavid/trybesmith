6e6f1c1d3be6b5e7d73511b7f102da86eb93f2d7
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-06-23 15:49:36 -0300

    feat: created user controller

---
+++ src/controllers/userController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import UserService from '../services/userService';
+
+class UserController {
+  constructor(private services = new UserService()) {}
+
+  public findUser = async (request: Request, response: Response) => {
+    const { username, password } = request.body;
+
+    const user = await this.services.findUser(username, password);
+
+    if (!user) return response.status(401).json({ message: 'Username or password invalid' });
+
+    return response.status(200).json({ token: response });
+  };
+
+  public create = async (request: Request, res: Response) => {
+    const { username, classe, level, password } = request.body;
+
+    const token = await this.services.create({ username, classe, level, password });
+
+    return res.status(201).json({ token });
+  };
+}
+
+export default UserController;
